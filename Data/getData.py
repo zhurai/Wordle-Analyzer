f5b2727494db2072e4adca00c8cd0493537d51c0
zhurai <zhurai.tsuki@gmail.com>
2022-05-12 22:14:41 -0700

    MVP for cloneGit
    updated gitignore to ignore the proper files
    updated the configuration ini to add the destination folder variable
    updated getData to clone the git repository (without checks yet)

--- Data/getData.py
+++ Data/getData.py
@@ -1,1 +1,26 @@
 # Python 3.10.4
+
+import config
+from git import Repo
+
+
+def cloneGitRepository(source,destination):
+	# check if "source" looks like a git repository
+	# if it is not, send to the other function
+
+	# if it is
+	Repo.clone_from(source,destination)
+
+
+# main function
+def main():
+
+	# clone the git repository
+	configsource=config.config['GETDATA']['source']
+	configdest=config.config['GETDATA']['destination']
+	cloneGitRepository(configsource,configdest)
+
+# check for proper environment to run main() function automatically
+if __name__ == '__main__':
+	main()
+
