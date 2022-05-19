4af4e8aece61c54931bda936dadf304b5757a831
zhurai <zhurai.tsuki@gmail.com>
2022-05-18 19:52:04 -0700

    [Data] importData.py
    initial commit

---
+++ Data/importData.py
@@ -0,0 +1,15 @@
+# Python 3.10.4
+import config
+import sys
+import getData
+
+def saveFile():
+    pass
+
+# main function
+def main():
+	pass
+
+# check for proper environment to run main() function automatically
+if __name__ == '__main__':
+	main()
