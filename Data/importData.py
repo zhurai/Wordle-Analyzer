3708b7cdfd0c1087dc307d3004b65e7aa183c0fc
zhurai <zhurai.tsuki@gmail.com>
2022-05-18 20:08:28 -0700

    [Data] implement #2 in importdata.py
    solves #2

--- Data/importData.py
+++ Data/importData.py
@@ -3,12 +3,25 @@ import config
 import sys
 import getData
 
-def saveFile():
-    pass
+def saveFile(TheData,TargetFile):
+    # TheData = list of words
+    # TargetFile = where to save into
+
+    # Erase previous contents of the file
+    config.debug("Erasing contents of file: " + TargetFile)
+    file = open(TargetFile,"w+")
+    file.truncate(0)
+    file.close()
+
+    # Save new data into the file
+    config.debug("Updating contents of file: " + TargetFile)
+    with open(TargetFile,'w+') as f:
+        for line in TheData:
+            f.write(line+"\n")
 
 # main function
 def main():
-	pass
+    pass
 
 # check for proper environment to run main() function automatically
 if __name__ == '__main__':
