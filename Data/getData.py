d97e0926b12443083709e57bf306d899bdb8133d
zhurai <zhurai.tsuki@gmail.com>
2022-05-24 22:27:24 -0700

    Data: minor fix to previous version
    - initialize count at beginning (I was declared prior to loop originally in my test scripts)
    - fixed temporary variable from test script

--- Data/getData.py
+++ Data/getData.py
@@ -11,11 +11,11 @@ def getInformation(thedata,length):
 
 	# Number of words containing each letter of the word at least once
 	for letter in list(string.ascii_lowercase):
-		for item in x:
+		count=0
+		for item in thedata:
 			if item.find(letter) > -1:
 				count=count+1
 		theinfo['once-'+letter]=count
-		count=0
 	
 	return theinfo
 
